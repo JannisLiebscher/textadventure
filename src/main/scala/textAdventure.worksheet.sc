57c6a2e31d41991bee869e12c9fab424a2b64ac0
Jannis Liebscher <jannis-liebscher@gmx.de>
2021-10-29 11:56:56 +0200

    geringverdiener

--- src/main/scala/textAdventure.worksheet.sc
+++ src/main/scala/textAdventure.worksheet.sc
@@ -1,5 +1,5 @@
 case class cells(cellwidth: Int) {
-  var value = 0
+  val value = 1
   def isSet: Boolean = value != 0
 }
 
---
+++ src/test/textAdventureSpec.scala
@@ -0,0 +1,4 @@
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatest.matchers.should.Matchers._
+
+class textAdventureSpec {}
