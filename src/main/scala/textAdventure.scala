dedda6c57002f3e8bc9aee850ad0ad554a82ab3b
Jannis Liebscher <jannis-liebscher@gmx.de>
2021-10-29 12:55:03 +0200

    spec

--- src/main/scala/textAdventure.scala
+++ src/main/scala/textAdventure.scala
@@ -2,12 +2,17 @@ object textAdventure {
   @main def hello: Unit =
     println("Welcome!")
 
+  val health = 10
   val width = 1
   val zeilen = 1
   val spalten = 20
   def cells(cellwidth: Int = 3) = ("|" + " " * cellwidth) * spalten + "|\n"
   def bar(cellwidth: Int = 3) = ("+" + "-" * cellwidth) * spalten + "+\n"
+
+  def healthbar(hp: Int = 0) = ("|" + "+ " * hp) + "  " * (10 - hp) + "|\n"
+
   val mesh = (bar(width) + cells(width)) * zeilen + bar(width)
   println(mesh)
-  println("Hey")
+  print("HP:" + healthbar(health))
+
 }
--- src/test/textAdventureSpec.scala
+++ src/test/textAdventureSpec.scala
@@ -1,4 +1,18 @@
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
-class textAdventureSpec {}
+class textAdventureSpec {
+
+  "A Healthbar" when {
+    "empty" should {
+      "be created using the amount of HP between 0 and 10" in {
+        val hp = healthbar(0)
+        hp should be("|                    |")
+      }
+    }
+    "full" should {
+      val hp = healthbar(10)
+      hp should be("|+ + + + + + + + + +|")
+    }
+  }
+}
